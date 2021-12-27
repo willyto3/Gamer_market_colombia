ee1954c3836f7b43ae85e486f038d30fe2b1be21
Willy Corzo Lubo <82851253+willyto3@users.noreply.github.com>
2021-12-27 11:35:29 -0500

    Se guarda el Teclado Pulsar

--- store/admin.py
+++ store/admin.py
@@ -8,7 +8,7 @@ from store.models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
-    prepopulate_fields = {'slug': ('name')}
+    prepopulated_fields = {'slug': ('name',)}
 
 
 @admin.register(Product)
@@ -16,4 +16,4 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'in_stock', 'created', 'updated']
     list_filter = ['in_stock', 'is_active']
     list_editable = ['price', 'in_stock']
-    prepopulate_fields = {'slug': ('title')}
+    prepopulated_fields = {'slug': ('title',)}
